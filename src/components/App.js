558b066539c88659c63d89fdf70e357221966d2c
sunga-lee <sungajunior1@gmail.com>
2023-04-22 16:20:08 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React  from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = (TASKS)
+  // const [categories, setCategories] = useState(CATEGORIES)
+  const [category, setCategory] = ("All");
+
+  // filtering throu tha task and category
+  const filteredTasks = tasks.filter(
+    (task) => category ===  "All" || task.category === category
+  );
+
+// task remove button
+  const removeTask = (text) => {
+    const newTask = tasks.filter((task) => task.text !== text);
+    console.log(text)
+    setTasks(newTask)
+  };
+
+   function onTaskFormSubmit(newTask) {
+     setTasks([...tasks, newTask]);
+   }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        category={category}
+        selectCategory={setCategory}
+      />
+      <NewTaskForm
+        categories={CATEGORIES.filter((category) => category !== "All")}
+        onTaskFormSubmit={onTaskFormSubmit}
+      />
+      <TaskList tasks={filteredTasks} removeTask={removeTask} />
     </div>
   );
 }
-
 export default App;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, removeTask}) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {tasks.map((task) => (<Task key={tasks.text} 
+        text={task.text}
+        category={task.category} 
+        removeTask={removeTask}
+        />))
+      }
     </div>
   );
 }
